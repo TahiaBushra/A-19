ab5d68c5d66039889ca49f2ff376fdd948717577
TahiaBushra <bushratahia@gmail.com>
2023-12-05 22:04:15 +0600

    Distructuring Done

---
+++ app.js
@@ -0,0 +1,17 @@
+"use strict"
+//  Distructuring
+
+// Example-1
+// const numbers = [1, 58, 30, 54, 89];
+// // const myNumber = numbers[1];
+// const myNumber = numbers.at(-2);
+// console.log(myNumber);
+// const [num1, , b, d, num5, p] = numbers;
+// console.log(num1, b, num5, d);
+
+// Swicthing Result
+// let a = 5;
+// let b = 10;
+// [a, b] = [b, a];
+// console.log(a, b);
+
