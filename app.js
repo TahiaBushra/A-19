d008524f4a38418ce8cd05ad245f2c0475ec1d2e
TahiaBushra <bushratahia@gmail.com>
2023-12-05 22:23:36 +0600

    Object Distructuring Done

--- app.js
+++ app.js
@@ -18,12 +18,35 @@
 // Nested Array
 
 // Example-1
-const myArray = [23, 50, 45, [85, 27]];
-const [a, c, , [l]] = myArray;
-console.log(a, c, l);
+// const myArray = [23, 50, 45, [85, 27]];
+// const [a, c, , [l]] = myArray;
+// console.log(a, c, l);
 
-// Example-2
-const myFriend = ["Era", "Lili", ["Tom", "Jonas", ["Piter", "Mathew"]]];
-const [frnd1, frnd2, [frnd3, frnd4, [frnd5]]] = myFriend;
-console.log(frnd1, frnd3, frnd5);
+// // Example-2
+// const myFriend = ["Era", "Lili", ["Tom", "Jonas", ["Piter", "Mathew"]]];
+// const [frnd1, frnd2, [frnd3, frnd4, [frnd5]]] = myFriend;
+// console.log(frnd1, frnd3, frnd5);
 
+// Object Distructuring
+
+// Examle-1
+// const animal = {
+//     animalName: "Dog",
+//     age: 5,
+//     foods: ["Meat", "Dog Food"],
+// };
+// const { animalName, foods: [food1, food2] } = animal;
+// console.log(animalName, food1);
+
+// Examle-2
+// const animal = {
+//     animalName: "Dog",
+//     age: 5,
+//     foods: ["Meat", "Dog Food"],
+//     enemies: {
+//         enemy1: "Cat",
+//         enemy2: "Bugs",
+//     },
+// };
+// const { animalName: petName, age: petAge, foods: [food1, food2], enemies: { enemy2: Disgusting } } = animal;
+// console.log(petName, petAge, food1, Disgusting);
